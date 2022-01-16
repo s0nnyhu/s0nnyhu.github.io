eded9a866a91c07e0ccd68915ceac5d9e99ead69
s0nnyhu <husonny1@gmail.com>
2022-01-16 18:02:59 +0100

    Add Main component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Main />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Main,
+  },
+};
 </script>
 
 <style>
@@ -22,5 +21,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  display: flex;
+  margin-top: 1rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+section {
+  margin: 0.5rem;
+}
+
+.social a {
+  padding: 0.5rem;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <section>
+      <h1>Nothing here yet</h1>
+    </section>
+    <section class="social">
+      <a href="https://github.com/s0nnyhu">Github</a>
+      <a href="https://fr.linkedin.com/in/sonny-hu-16001384">LinkekIn</a>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
